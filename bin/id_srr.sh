0fb76b03ffd999aec9ec979a61d92e6f95e53d0d
Tung H. Nguyen <111020529+tungwinxp@users.noreply.github.com>
2024-12-01 03:02:58 +0000

    speed up xml query

--- bin/id_srr.sh
+++ bin/id_srr.sh
@@ -21,7 +21,7 @@ echo "Getting all SRR/ERR/DRR from NCBI ID (SRP or Bioproject, etc.) $SRA"
 # Fetch and process SRR/ERR/DRR accessions
 curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=sra&term=${SRA}&retmode=xml&usehistory=y" \
 | xmllint --xpath "concat(//QueryKey, ' ', //WebEnv)" - \
-| awk '{print "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=sra&query_key="$1"&WebEnv="$2"&rettype=runinfo"}' \
+| awk '{print "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=sra&query_key="$1"&WebEnv="$2"&rettype=runinfo&retmax=1000000000"}' \
 | xargs curl -s -o ${SRA}_RunInfo.xml
 
 if [[ $? -ne 0 ]]; then
@@ -30,16 +30,8 @@ if [[ $? -ne 0 ]]; then
 fi
 
 # Extract the raw Run IDs from the XML file
-RAW_IDS=$(xmllint --xpath "//Run/text()" ${SRA}_RunInfo.xml 2>/dev/null)
+${SRA}_RunInfo.xml | grep \<Run\> | grep "<Run>" | sed 's/.*<Run>\(.*\)<\/Run>.*/\1/' >| $OUTPUT_FILE
 
-# Check if the output is a single long line with multiple "RR" patterns
-if [[ $(echo "$RAW_IDS" | grep -o "RR[0-9]\+" | wc -l) -gt 1 ]]; then
-    echo "$RAW_IDS" | sed 's/\([A-Z]*RR[0-9]*\)/\1\n/g' | grep -E 'RR[0-9]+' > "$OUTPUT_FILE"
-    echo "Accessions successfully processed and written to $OUTPUT_FILE."
-else
-    echo "$RAW_IDS" > "$OUTPUT_FILE"
-    echo "Single accession found. Output written to $OUTPUT_FILE."
-fi
 
 # Validate output
 if [[ -s "$OUTPUT_FILE" ]]; then
